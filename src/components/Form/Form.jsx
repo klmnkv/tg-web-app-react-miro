329089a591a1b2737486e0349313a787427ec52e
pomka <kolomnikov0405@gmail.com>
2024-02-24 19:26:37 +0700

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css';
-import {userTelegram} from "../../hooks/userTelegram";
+import {userTelegram, userTelegram} from "../../hooks/userTelegram";
+
 const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
@@ -8,18 +9,18 @@ const Form = () => {
     const {tg} = userTelegram();
 
     const onSendData = useCallback(() => {
-        const data =  {
+        const data = {
             country,
             street,
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [country,street,subject])
+    }, [country, street, subject])
 
-    useEffect(() =>{
-        tg.onEvent('mainButtonClicked', onSendData())
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData())
+            tg.offEvent('mainButtonClicked', onSendData)
         }
     }, [onSendData])
 
@@ -40,24 +41,27 @@ const Form = () => {
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
     }
-        const onChangeStreet = (e) => {
+
+    const onChangeStreet = (e) => {
         setStreet(e.target.value)
     }
-        const onChangeSubject = (e) => {
+
+    const onChangeSubject = (e) => {
         setSubject(e.target.value)
     }
+
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
-                className={"input"}
+                className={'input'}
                 type="text"
                 placeholder={'Страна'}
                 value={country}
                 onChange={onChangeCountry}
             />
             <input
-                className={"input"}
+                className={'input'}
                 type="text"
                 placeholder={'Улица'}
                 value={street}
